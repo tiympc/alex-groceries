3cb39d922c926e2c718586d59e6023aa22a6ee5d
asahinidis <asahinidis@gmail.com>
2024-05-02 11:50:47 -0700

    update

--- shopping-list-main/script.js
+++
@@ -1,6 +0,0 @@
-
-
-clearButton = document.querySelector('#clear');
-
-clearButton.addEventListener('click', () => alert('cleared'));
-clearButton.addEventListener('click', () => console.log('hi'));
